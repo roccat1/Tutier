3b234eb8e2f1d1fa17ab9e89d32b8069c5ab21f7
roccat1 <roc.r2005@gmail.com>
2021-12-12 10:14:14 +0100

    fix

--- src/config/dbConnections.js
+++ src/config/dbConnections.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const fs = require('fs');
 const path = require('path');
 
-let DBPassword = fs.readFileSync(path.join(__dirname, '../../DBPassword.txt'), 'utf-8');
+let DBPassword = fs.readFileSync(path.join(__dirname, '../DBPassword.txt'), 'utf-8');
 
 module.exports = () => {
     return mysql.createConnection({
